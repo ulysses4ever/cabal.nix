e22b21ede210a50e8128c7c503e108acdc89393a
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-04-23 11:14:06 -0400

    move to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
